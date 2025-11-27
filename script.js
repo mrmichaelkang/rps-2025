81dcbb19264d5847146a21af3de48732fc40e4f1
Michael Kang <52021323+mrmichaelkang@users.noreply.github.com>
2025-11-26 19:08:58 -0800

    Updated script - reset logic; wrote logic to handle resetting game

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const options = document.querySelector("#options");
 let userScore = 0;
 let cpuScore = 0;
+const userScoreDisplay = document.querySelector(".user-score");
+const cpuScoreDisplay = document.querySelector(".cpu-score");
+const resetBtn = document.querySelector(".reset");
+const infoText = document.querySelector(".info-text");
+let message = "";
 let userChoice = "";
+
 
 options.addEventListener("click", event => {
   let target = event.target;
-
+  event.stopPropagation();
   if(target.classList.contains("btn")){
     userChoice = target.id;
     playGame();
   }
 });
+
+resetBtn.addEventListener("click", resetGame);
+
+function resetGame() {
+  infoText.textContent = "Welcome to Rock, Paper, Scissors! Please select your option to begin!";
+  cpuScore = 0
+  userScore = 0;
+  userScoreDisplay.textContent = userScore;
+  cpuScoreDisplay.textContent = cpuScore;
+  userChoice = "";
+  message = "";
+  resetBtn.classList.add("hide");
+}
 
 function getComputerChoice() {
   const cpuOptions = ["Rock", "Paper", "Scissors"];
@@ -21,34 +40,35 @@ function getComputerChoice() {
 function playRound(userChoice, cpuChoice) {
   // Check for userChoice equals rock
   if(userChoice === "Rock" && cpuChoice === "Paper") {
-    console.log(`You lose! ${cpuChoice} beats ${userChoice}!`);
+    message = `You lose! ${cpuChoice} beats ${userChoice}!`;
     cpuScore += 1;
   } else if(userChoice == "Rock" && cpuChoice === "Scissors") {
-    console.log(`You win! ${userChoice} beats ${cpuChoice}!`);
+     message = `You win! ${userChoice} beats ${cpuChoice}!`;
     userScore += 1;
   } else if(userChoice === "Paper" && cpuChoice === "Scissors") {
-    console.log(`You lose! ${cpuChoice} beats ${userChoice}!`);
+     message = `You lose! ${cpuChoice} beats ${userChoice}!`;
     cpuScore += 1;
   } else if(userChoice == "Paper" && cpuChoice === "Rock") {
-    console.log(`You win! ${userChoice} beats ${cpuChoice}!`);
+     message = `You win! ${userChoice} beats ${cpuChoice}!`;
     userScore += 1;
   } else if(userChoice === "Scissors" && cpuChoice === "Rock") {
-    console.log(`You lose! ${cpuChoice} beats ${userChoice}!`);
+     message = `You lose! ${cpuChoice} beats ${userChoice}!`;
     cpuScore += 1;
   } else if(userChoice == "Scissors" && cpuChoice === "Paper") {
-    console.log(`You win! ${userChoice} beats ${cpuChoice}!`);
+     message = `You win! ${userChoice} beats ${cpuChoice}!`;
     userScore += 1;
   } else {
-    console.log("It's a draw!");
+     message = "It's a draw!";
   }
+  userScoreDisplay.textContent = userScore;
+  cpuScoreDisplay.textContent = cpuScore;
+  infoText.textContent = message + " Select an option to continue playing"
+  resetBtn.classList.remove("hide");
 }
 
 function playGame() {
   let cpuChoice;
-
   cpuChoice = getComputerChoice();
-  playRound(userChoice, cpuChoice);
-  console.log(`User score: ${userScore}\nCPU Score: ${cpuScore}`);
 
   playRound(userChoice, cpuChoice);
 }
